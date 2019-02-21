769c0bdbd770649db1dfbb860b8a6dbfdb6b29e9
championway <cpwearth.eed03@g2.nctu.edu.tw>
2019-02-21 16:05:14 +0800

    for subt training

--- data/config.py
+++ data/config.py
@@ -70,3 +70,18 @@ subt = {
     'clip': True,
     'name': 'subt',
 }
+
+person = {
+    'num_classes': 2,
+    'lr_steps': (4000, 8000, 12000),
+    'max_iter': 15000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [21, 45, 99, 153, 207, 261],
+    'max_sizes': [45, 99, 153, 207, 261, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'person',
+}
